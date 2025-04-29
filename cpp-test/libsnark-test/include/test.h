0f07385cbc6ad4b65e35b77618c35b02fc8d25dd
AlexDavisNeuwiem <alexdavisneuwiem@gmail.com>
2025-04-29 13:21:14 -0300

    Adiciona o teste não funcional da libsnark

---
+++ cpp-test/libsnark-test/include/test.h
@@ -0,0 +1,12 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <string>
+
+// Função que soma dois inteiros
+int soma(int a, int b);
+
+// Função que imprime uma mensagem
+void imprime_mensagem(const std::string& mensagem);
+
+#endif // TEST_H
---
+++ cpp-test/libsnark-test/src/test.cpp
@@ -0,0 +1,10 @@
+#include "test.h"
+#include <iostream>
+
+int soma(int a, int b) {
+    return a + b;
+}
+
+void imprime_mensagem(const std::string& mensagem) {
+    std::cout << "Mensagem: " << mensagem << std::endl;
+}
